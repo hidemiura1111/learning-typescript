de6b375548ed702bf91231b69ebd0656a6bb883a
hidems <miura.eleven@gmail.com>
2023-01-02 21:41:14 +0100

    Type and Enum for TypeAcript

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ var User = /** @class */ (function () {
     return User;
 }());
 console.log("hello world");
+// Type
+// number, string, boolean, any
+var i1 = 10;
+var i2 = 10; // i: number
+var x; // var x: any
+x = 10;
+x = "hello";
+var results;
+results = [10, 5, 3];
+// enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 3] = "Green";
+    Color[Color["Blue"] = 4] = "Blue";
+})(Color || (Color = {}));
+console.log(Color.Blue);
+(function (Color) {
+    Color[Color["Yellow"] = 6] = "Yellow";
+})(Color || (Color = {}));
+console.log(Color['Yellow']);
+console.log(Color[3]);
--- main.ts
+++ main.ts
@@ -2,3 +2,34 @@ class User {
 
 }
 console.log("hello world");
+
+// Type
+// number, string, boolean, any
+
+var i1: number = 10;
+var i2 = 10; // i: number
+
+var x; // var x: any
+x = 10;
+x = "hello";
+
+var results: number[];
+results = [10, 5, 3];
+
+
+// enum
+enum Color {
+  Red, // 0
+  Green = 3,
+  Blue, // 4
+}
+
+console.log(Color.Blue);
+
+enum Color {
+  Yellow = 6,
+}
+
+console.log(Color['Yellow']);
+console.log(Color[3]);
+
