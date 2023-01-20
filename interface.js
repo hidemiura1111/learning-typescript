eb4d6f161d9d441fadfe06d3d7f97277553d3c5d
hidems <miura.eleven@gmail.com>
2023-01-20 23:06:45 +0100

    Interface extend

--- interface.js
+++ interface.js
@@ -1,10 +1,22 @@
 // Interface
 function getTotal(result) {
-    return result.a + result.b;
+    if (result.c) {
+        return result.a + result.b + result.c;
+    }
+    else {
+        return result.a + result.b;
+    }
 }
-var result = {
+var result_1 = {
     a: 10,
     b: 20,
-    c: 'Hello'
+    c: 30,
+    name: 'John'
 };
-console.log(getTotal(result));
+var result_2 = {
+    a: 10,
+    b: 20,
+    name: 'Jane'
+};
+console.log(getTotal(result_1));
+console.log(getTotal(result_2));
--- interface.ts
+++ interface.ts
@@ -1,18 +1,37 @@
 // Interface
 
-interface Result {
+interface FirstResult {
   a: number;
+}
+
+interface SecondResult {
   b: number;
 }
 
-function getTotal(result: Result) {
-  return result.a + result.b;
+interface Result extends FirstResult, SecondResult {
+  c?: number;
 }
 
-var result = {
+function getTotal(result: Result) {
+  if (result.c) {
+    return result.a + result.b + result.c;
+  } else {
+    return result.a + result.b;
+  }
+}
+
+var result_1 = {
   a: 10,
   b: 20,
-  c: 'Hello',
+  c: 30,
+  name: 'John',
 };
 
-console.log(getTotal(result));
+var result_2 = {
+  a: 10,
+  b: 20,
+  name: 'Jane',
+};
+
+console.log(getTotal(result_1));
+console.log(getTotal(result_2));
